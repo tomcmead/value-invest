a51d7b84aed2e60be208ff9f57b92eb9baf1b9c0
Tom Mead <tomcmead0@gmail.com>
2024-04-17 15:48:22 +0100

    Add StockData skeleton class to CMake build

---
+++ lib/stock_data/include/stock_data.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/// @brief Get historical stock data
+/// 'facade' for 'Alpha Vantage API' financial data
+class StockData
+{
+
+};
---
+++ lib/stock_data/src/stock_data.cpp
@@ -0,0 +1,5 @@
+#include <spdlog/spdlog.h>
+#include <string>
+#include "stock_data.h"
+#include "curl_handler.h"
+
