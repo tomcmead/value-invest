02331b7d5f6c73b7d99853e29813fd9375f18520
Tom Mead <tomcmead0@gmail.com>
2024-04-17 14:04:24 +0100

    Add CurlHandler set-up + basic HttpGet functions

--- lib/curl_handler/include/curl_handler.h
+++ lib/curl_handler/include/curl_handler.h
@@ -3,18 +3,21 @@
 #include <curl/curl.h>
 
 /// @brief cURL library API handler.
-/// 'Singeleton' and 'Facade' design patterns
+/// 'Singleton' and 'Facade' design patterns
 class CurlHandler
 {
 public:
     CurlHandler(CurlHandler const&) = delete;
     CurlHandler& operator=(CurlHandler const&) = delete;
     virtual ~CurlHandler();
-
     static CurlHandler* GetInstance();
     static void DeleteInstance();
+
+    CURLcode PerformHttpGet();
 private:
     CurlHandler();
+    static size_t CurlWriteCallback(void *buffer, size_t size, size_t nmemb, void *userp);
+
     static CurlHandler* curl_handler_instance;
     CURL* curl_handle;
 };
--- lib/curl_handler/src/curl_handler.cpp
+++ lib/curl_handler/src/curl_handler.cpp
@@ -1,20 +1,33 @@
 #include <curl/curl.h>
+#include <spdlog/spdlog.h>
+#include <string>
 #include "curl_handler.h"
 
 CurlHandler* CurlHandler::curl_handler_instance = nullptr; // singleton
+
+namespace
+{
+    const std::string kRemote_endpoint_url = "www.google.com";
+}
 
 /// @brief Initialise cURL library
 CurlHandler::CurlHandler()
 {
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl_handle = curl_easy_init();
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, CurlWriteCallback);
+    spdlog::info("CurlHandler Configured");
 }
 
 /// @brief Cleanup cURL library data
 CurlHandler::~CurlHandler()
 {
-    curl_easy_cleanup(curl_handle);
+    if(curl_handle != nullptr)
+    {
+        curl_easy_cleanup(curl_handle);
+    }
     curl_global_cleanup();
+    spdlog::info("CurlHandler Destroyed");
 }
 
 /// @brief Create singleton CurlHandler instance
@@ -35,4 +48,17 @@ void CurlHandler::DeleteInstance()
     {
         delete curl_handler_instance;
     }
+}
+
+CURLcode CurlHandler::PerformHttpGet()
+{
+    curl_easy_setopt(curl_handle, CURLOPT_URL, kRemote_endpoint_url.c_str());
+    CURLcode res = curl_easy_perform(curl_handle);
+    spdlog::info("HTTP Status code: {0:d}", static_cast<int>(res));
+    return res;
+}
+
+size_t CurlHandler::CurlWriteCallback(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+   return size * nmemb;
 }
