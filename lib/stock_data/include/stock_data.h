cacb0d092532064c6950170849db5ccc62c3c158
Tom Mead <tomcmead0@gmail.com>
2024-04-17 17:00:01 +0100

    Add StockData set-up get env var API key for Alpha Vantage financial data

--- lib/stock_data/include/stock_data.h
+++ lib/stock_data/include/stock_data.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include "curl_handler.h"
+
 /// @brief Get historical stock data
 /// 'facade' for 'Alpha Vantage API' financial data
 class StockData
 {
-
+public:
+    StockData();
+private:
+    CurlHandler* curl_handle;
+    int alpha_vantage_api_key;
 };
--- lib/stock_data/src/stock_data.cpp
+++ lib/stock_data/src/stock_data.cpp
@@ -1,5 +1,25 @@
 #include <spdlog/spdlog.h>
 #include <string>
+#include <cstdlib>
 #include "stock_data.h"
 #include "curl_handler.h"
 
+namespace
+{
+    const std::string kAlpha_vantage_api_key_env_var = "ALPHA_VANTAGE_API_KEY";
+}
+
+StockData::StockData()
+{
+    curl_handle = CurlHandler::GetInstance();
+
+    alpha_vantage_api_key = std::stoi(std::getenv(kAlpha_vantage_api_key_env_var.c_str()));
+
+    if(!alpha_vantage_api_key)
+    {
+        spdlog::critical("StockData::StockData evironment variable '" + kAlpha_vantage_api_key_env_var + "' is not set");
+        return;
+    }
+
+    spdlog::info("Alpha Vantage API Key: {0:d}", alpha_vantage_api_key);
+}
