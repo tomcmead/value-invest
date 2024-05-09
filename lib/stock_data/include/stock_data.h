9faa1dfa6fec14fb64be54c641a521cf84ab1077
Tom Mead <tomcmead0@gmail.com>
2024-05-09 11:57:37 +0100

    Refactor StockData with templated functions

--- lib/stock_data/include/stock_data.h
+++ lib/stock_data/include/stock_data.h
@@ -5,15 +5,23 @@
 #include "json_parser.h"
 #include "income_statement.h"
 
+namespace stock_data_api
+{
+    const long kHttp_ok = 200;
+    const std::string kAlpha_vantage_api_key_env_var = "ALPHA_VANTAGE_API_KEY";
+    const std::string kIncome_statement_api = "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=";
+}
+
 /// @brief Get historical stock data
 /// 'facade' for 'Alpha Vantage API' financial data
 class StockData
 {
 public:
     StockData();
+    template<typename TFinancialType>
     void GetFinancialData(std::string symbol,
         FundamentalFinancialType fundamental_type,
-        IncomeStatement& financial_data);
+        TFinancialType& financial_data);
 
 private:
     CurlHandler* curl_handle;
@@ -21,7 +29,52 @@ private:
 
     std::string GetApiFundamentalData(std::string symbol,
         FundamentalFinancialType fundamental_type);
+    template<typename TFinancialType>
     bool ParseFundamentalData(std::string fundamental_data,
         FundamentalFinancialType fundamental_type,
-        IncomeStatement& financial_data);
+        TFinancialType& financial_data);
 };
+
+/// @brief Generate FinancialData struct of requested symbol and financial type
+/// @param symbol stock ticker
+/// @param fundamental_type enum of finanical data type
+/// @param financial_data reference to FinancialData
+/// @return bool 0=success, 1=fail
+template<typename TFinancialType>
+void StockData::GetFinancialData(std::string symbol,
+    FundamentalFinancialType fundamental_type,
+    TFinancialType& financial_data)
+{
+    spdlog::info("StockData::GetFinancialData");
+
+    if(alpha_vantage_api_key.empty() == true)
+    {
+        spdlog::critical("StockData::GetFinancial evironment variable '" + stock_data_api::kAlpha_vantage_api_key_env_var + "' is not set");
+    }
+
+    std::string fundamental_data = GetApiFundamentalData(symbol, fundamental_type);
+    ParseFundamentalData<TFinancialType>(fundamental_data, fundamental_type, financial_data);
+}
+
+/// @brief Convert raw JSON API string data to FinancialData struct
+/// @param fundamental_data JSON string
+/// @param fundamental_type enum of finanical data type
+/// @param financial_data& reference to completed FinancialData struct
+/// @return bool 0=success, 1=fail
+template<typename TFinancialType>
+bool StockData::ParseFundamentalData(std::string fundamental_data,
+    FundamentalFinancialType fundamental_type,
+    TFinancialType& financial_data)
+{
+    spdlog::info("StockData::ParseFundamentalData");
+
+    // std::unique_ptr<JsonParser> json_parser(new JsonParser);
+    // bool parse_fail = json_parser->GetFinancial(fundamental_data, fundamental_type, financial_data);
+    JsonParser json_parser;
+    bool parse_fail = json_parser.GetFinancial<TFinancialType>(fundamental_data, fundamental_type, financial_data);
+    if(parse_fail == true)
+    {
+        spdlog::critical("StockData::ParseFundamentalData JSON parsing failed");
+    }
+    return parse_fail;
+}
--- lib/stock_data/src/stock_data.cpp
+++ lib/stock_data/src/stock_data.cpp
@@ -7,13 +7,6 @@
 #include "income_statement.h"
 #include "json_parser.h"
 
-namespace
-{
-    const long kHttp_ok = 200;
-    const std::string kAlpha_vantage_api_key_env_var = "ALPHA_VANTAGE_API_KEY";
-    const std::string kIncome_statement_api = "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=";
-}
-
 /// @brief Configure CurlHandler and Alpha Vantage API key
 StockData::StockData()
 {
@@ -22,33 +15,13 @@ StockData::StockData()
     curl_handle = CurlHandler::GetInstance();
     alpha_vantage_api_key = "";
 
-    if(std::getenv(kAlpha_vantage_api_key_env_var.c_str()) == NULL)
+    if(std::getenv(stock_data_api::kAlpha_vantage_api_key_env_var.c_str()) == NULL)
     {
-        spdlog::critical("StockData::StockData evironment variable '" + kAlpha_vantage_api_key_env_var + "' is not set");
+        spdlog::critical("StockData::StockData evironment variable '" + stock_data_api::kAlpha_vantage_api_key_env_var + "' is not set");
         return;
     }
-    alpha_vantage_api_key = std::getenv(kAlpha_vantage_api_key_env_var.c_str());
+    alpha_vantage_api_key = std::getenv(stock_data_api::kAlpha_vantage_api_key_env_var.c_str());
     spdlog::info("StockData::StockData Alpha Vantage API Key: {}", alpha_vantage_api_key);
-}
-
-/// @brief Generate FinancialData struct of requested symbol and financial type
-/// @param symbol stock ticker
-/// @param fundamental_type enum of finanical data type
-/// @param financial_data reference to FinancialData
-/// @return bool 0=success, 1=fail
-void StockData::GetFinancialData(std::string symbol,
-    FundamentalFinancialType fundamental_type,
-    IncomeStatement& financial_data)
-{
-    spdlog::info("StockData::GetFinancialData");
-
-    if(alpha_vantage_api_key.empty() == true)
-    {
-        spdlog::critical("StockData::GetFinancial evironment variable '" + kAlpha_vantage_api_key_env_var + "' is not set");
-    }
-
-    std::string fundamental_data = GetApiFundamentalData(symbol, fundamental_type);
-    ParseFundamentalData(fundamental_data, fundamental_type, financial_data);
 }
 
 /// @brief Gets raw API JSON data response as string
@@ -66,7 +39,7 @@ std::string StockData::GetApiFundamentalData(std::string symbol,
     switch(fundamental_type)
     {
         case kIncomeStatement:
-            api_instruction.append(kIncome_statement_api);
+            api_instruction.append(stock_data_api::kIncome_statement_api);
             break;
     }
     api_instruction.append(symbol);
@@ -75,7 +48,7 @@ std::string StockData::GetApiFundamentalData(std::string symbol,
     try
     {
         long http_code = curl_handle->PerformHttpGet(api_instruction, http_response);
-        if(http_code != kHttp_ok)
+        if(http_code != stock_data_api::kHttp_ok)
         {
             std::runtime_error("StockData::GetApiFinancialData " + symbol +
                 " " + std::to_string(fundamental_type) + " HTTP request failed with HTTP Code: "
@@ -89,26 +62,4 @@ std::string StockData::GetApiFundamentalData(std::string symbol,
     }
     spdlog::info("StockData::GetApiFinancialData {} {} HTTP GET request successful", symbol, std::to_string(fundamental_type));
     return http_response;
-}
-
-/// @brief Convert raw JSON API string data to FinancialData struct
-/// @param fundamental_data JSON string
-/// @param fundamental_type enum of finanical data type
-/// @param financial_data& reference to completed FinancialData struct
-/// @return bool 0=success, 1=fail
-bool StockData::ParseFundamentalData(std::string fundamental_data,
-    FundamentalFinancialType fundamental_type,
-    IncomeStatement& financial_data)
-{
-    spdlog::info("StockData::ParseFundamentalData");
-
-    // std::unique_ptr<JsonParser> json_parser(new JsonParser);
-    // bool parse_fail = json_parser->GetFinancial(fundamental_data, fundamental_type, financial_data);
-    JsonParser json_parser;
-    bool parse_fail = json_parser.GetFinancial<IncomeStatement>(fundamental_data, fundamental_type, financial_data);
-    if(parse_fail == true)
-    {
-        spdlog::critical("StockData::ParseFundamentalData JSON parsing failed");
-    }
-    return parse_fail;
 }
