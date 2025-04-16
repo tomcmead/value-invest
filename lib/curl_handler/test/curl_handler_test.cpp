8f3c196ef1a6e295b3f97bb812446c8a5587ee17
Tom Mead <tomcmead0@gmail.com>
2025-04-16 11:23:35 +0100

    Add CurlHandler Http Get Bad Gateway 502 test

--- lib/curl_handler/test/curl_handler_test.cpp
+++ lib/curl_handler/test/curl_handler_test.cpp
@@ -14,3 +14,10 @@ TEST(CurlHandler, GetResponsePerformHttpGet_Unauthorized_Return401) {
     curl_handle->PerformHttpGet("httpstat.us/401", response);
     EXPECT_EQ("401 Unauthorized", response);
 }
+
+TEST(CurlHandler, GetResponsePerformHttpGet_BadGateway_Return502) {
+    std::string response= "";
+    CurlHandler* curl_handle = CurlHandler::GetInstance();
+    curl_handle->PerformHttpGet("httpstat.us/502", response);
+    EXPECT_EQ("502 Bad Gateway", response);
+}
