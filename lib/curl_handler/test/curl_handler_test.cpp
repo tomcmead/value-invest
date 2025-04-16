a9c01f2ae8cda7f8e06166151bbf349b681e44b8
Tom Mead <tomcmead0@gmail.com>
2025-04-16 11:14:16 +0100

    Add CurlHandler Http Get unauthorised 401 test

--- lib/curl_handler/test/curl_handler_test.cpp
+++ lib/curl_handler/test/curl_handler_test.cpp
@@ -1,9 +1,16 @@
 #include "gtest/gtest.h"
 #include "curl_handler.h"
 
-TEST(CurlHandler, GetResponsePerformHttpGet_200_ReturnOkResponse) {
+TEST(CurlHandler, GetResponsePerformHttpGet_Ok_Return200) {
     std::string response= "";
     CurlHandler* curl_handle = CurlHandler::GetInstance();
     curl_handle->PerformHttpGet("httpstat.us/200", response);
     EXPECT_EQ("200 OK", response);
+}
+
+TEST(CurlHandler, GetResponsePerformHttpGet_Unauthorized_Return401) {
+    std::string response= "";
+    CurlHandler* curl_handle = CurlHandler::GetInstance();
+    curl_handle->PerformHttpGet("httpstat.us/401", response);
+    EXPECT_EQ("401 Unauthorized", response);
 }
