7c0867d10a430e808877c83c6fb8a2fc46237fe6
Tom Mead <tomcmead0@gmail.com>
2025-04-17 09:06:11 +0100

    Add example discounted cash flow stock valuation

--- src/value_invest.cpp
+++ src/value_invest.cpp
@@ -1,3 +1,15 @@
+#include "stock_valuation.h"
+#include "stock_data.h"
+
 int main()
 {
+    std::string stock_ticker = "AAPL";
+    float share_price = 200;
+    int forecast_years = 5;
+    float share_beta = 1;
+    float growth_percent = 5;
+
+    StockValuation stock_valuation;
+    stock_valuation.DiscountedCashFlow(stock_ticker, share_price, forecast_years,
+        share_beta, growth_percent);
 }
